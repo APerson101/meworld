cff9add759e06fb717180a59dd579025898c3548
Abdulhadi <abdulhadih48@gmail.co>
2022-09-07 11:07:42 +0100

    login screen

--- lib/views/screens/authentication/login_view.dart
+++ lib/views/screens/authentication/login_view.dart
@@ -1,12 +1,57 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
-
+  LoginView({Key? key}) : super(key: key);
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: _emailController,
+              validator: (email) {
+                if (email != null) {
+                  return EmailValidator.validate(email)
+                      ? null
+                      : 'Enter a valid email';
+                }
+                return 'No email was entered';
+              },
+              decoration: InputDecoration(
+                helperText: 'Enter email',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              obscureText: true,
+              controller: _passwordController,
+              onChanged: (password) {},
+              decoration: InputDecoration(
+                helperText: 'Enter Password',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                // Login button: validate detail
+                GoRouter.of(context).go('Main App');
+              },
+              child: const Text("Login"))
+        ],
+      ),
     );
   }
 }
