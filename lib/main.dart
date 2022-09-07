76142de31bde61ac2fd6837ba5d11a45c60d4202
Abdulhadi <abdulhadih48@gmail.co>
2022-09-07 12:34:40 +0100

    accountInfo

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meworld/views/screens/authentication/login_view.dart';
 import 'package:meworld/views/screens/authentication/signup_view.dart';
 import 'package:meworld/views/screens/main_app/main_app_view.dart';
+import 'package:meworld/views/screens/main_app/settings_view.dart';
+
+import 'views/screens/main_app/account_info_view.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -36,6 +39,10 @@ class MyApp extends StatelessWidget {
         path: '/MainApp',
         builder: (context, state) => const MainAppView(),
       ),
+      GoRoute(path: '/Settings', builder: (context, state) => SettingsView()),
+      GoRoute(
+          path: '/AccountInfo',
+          builder: (context, state) => const AccountInfoView()),
     ],
   );
 }
